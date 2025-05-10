83fd187cd5fea8625dbd2c95cb322aa0a1ccdd41
khiraki <129939301+Keisuke-Hiraki@users.noreply.github.com>
2025-05-10 21:51:39 +0900

    tailwind.config.ts追加

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        amazon: {
+          orange: '#FF9900',
+          navy: '#232F3E'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
